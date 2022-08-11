25b1928df5e8f636901959bf90bdd10d9bf3f27e
MATHIVATHANI B <103478368+mathivathani-b@users.noreply.github.com>
2022-08-11 10:45:27 +0530

    Filter

---
+++ Filter1/src/main/java/filter/Filt.java
@@ -0,0 +1,57 @@
+package filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class Filt
+ */
+public class Filt implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public Filt() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		PrintWriter pw=response.getWriter();
+		if(request.getParameter("user").equals(request.getParameter("pwd"))) {
+			chain.doFilter(request, response);	
+		}
+		else {
+			pw.print("check the credentials");
+		}
+	
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
