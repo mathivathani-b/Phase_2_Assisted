1c5606c5be80bb864ba4d329df92f90a6fe78db9
MATHIVATHANI B <103478368+mathivathani-b@users.noreply.github.com>
2022-08-11 10:56:23 +0530

    Get and post

---
+++ GET_and_post/src/main/java/Module_2/Post1.java
@@ -0,0 +1,48 @@
+package Module_2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Post1
+ */
+public class Post1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Post1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        out.println("Name=" + name + "<br>Address=" + address);
+        out.println("</body></html>");
+	}
+
+}
